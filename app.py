3cb9972de2ab46471cc9a0b1f5c0cf05e828d158
JMABrown <james.mich.alex.brown@gmail.com>
2026-02-12 15:53:26 +0000

    Testing dynamic web page content with datetime

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<center><img loading="lazy" src="chipmusic/chipmusic-discovery/SonicGame.png" alt="Sonic" width="56" height="85" border="0" align="top" title="Sonic"></center>'
+    return f'''
+    <h1>{str(datetime.datetime.now())}</h1>
+    <center>
+    <img loading="lazy" src="chipmusic/chipmusic-discovery/SonicGame.png" alt="Sonic" width="56" height="85" border="0" align="top" title="Sonic">
+    </center
+    '''
