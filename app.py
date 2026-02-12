0a48a5738a3cadc0c881c337af730f3b00d30b6e
JMABrown <james.mich.alex.brown@gmail.com>
2026-02-12 15:56:25 +0000

    Testing page visits

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ import datetime
 
 app = Flask(__name__)
 
+a = 0
+
 @app.route('/')
 def hello_world():
+    a += 1
     return f'''
     <h1>{str(datetime.datetime.now())}</h1>
+    <h2>Visits : {a}<h2>
     <center>
     <img loading="lazy" src="chipmusic/chipmusic-discovery/SonicGame.png" alt="Sonic" width="56" height="85" border="0" align="top" title="Sonic">
     </center
