089ad8c6cc4fb34308a5cfb3c3cbeb7f6792369d
JMABrown <james.mich.alex.brown@gmail.com>
2026-02-12 15:27:43 +0000

    Attempt to add sonic image

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! James was here :)'
+    return '<center><img loading="lazy" src="chipmusic/chipmusic-discovery/SonicGame.png" alt="Sonic" width="56" height="85" border="0" align="top" title="Sonic"></center>'
