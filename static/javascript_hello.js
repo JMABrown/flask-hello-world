feb5910a6b0b59189144d05848a7bb69e737615d
JMABrown <james.mich.alex.brown@gmail.com>
2026-02-13 10:32:58 +0000

    Making button change html since alert didnt appear to work

--- static/javascript_hello.js
+++ static/javascript_hello.js
@@ -3,5 +3,6 @@
 document.getElementById("output").innerHTML = "Hello World from javascript";
 
 document.getElementById("bye_button").onclick = function () {
-    alert('Goodbye!');
+    alert('Goodbye!'); //<- what does this do
+    document.getElementById("output").innerHTML = "Goodbye!";
 };
